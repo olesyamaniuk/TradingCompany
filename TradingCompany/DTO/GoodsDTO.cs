7133cc7e95a9db21f7d1059f0c9fd273c4da5a14
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2020-10-03 03:11:06 +0300

    Add files via upload

---
+++ TradingCompany/DTO/GoodsDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class GoodsDTO
+    {
+        public int GoodsID { get; set; }
+        public string GoodsName { get; set; }
+        public string GoodsPrice { get; set; }
+        public string GoodsDescription { get; set; }
+        public string GoodsAmount { get; set; }
+
+        public int SepetID { get; set; }
+
+    }
+}
---
+++ TradingCompany/DTO/PurchaseStatusDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PurchaseStatusDTO
+    {
+        public int ID { get; set; }
+        public string StatusName { get; set; }
+        public bool PurchaseStatus   { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
