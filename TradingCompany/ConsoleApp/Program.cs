f9a6c4a12e6d603bb453f91ffae5631c496deea0
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2020-12-10 16:01:22 +0200

    Add files via upload

---
+++ TradingCompany/ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DTO;
+using Dal;
+using ConsoleApplicationTC.Menu;
+using Dal.Concrete;
+
+
+namespace ConsoleApplicationTC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMenu My_Menu = new MyMenu();
+            int item;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter number: ");
+                    item = Convert.ToInt32(Console.ReadLine());
+
+                    
+                   if (My_Menu.Go_To(item))
+                    {
+                        return;
+                    }
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(exp.Message);
+                }
+
+            }
+            
+        }
+    }
+}
