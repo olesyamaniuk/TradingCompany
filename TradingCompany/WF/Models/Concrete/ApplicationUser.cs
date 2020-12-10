248dd40a7970faca3817f3ff9f4a2382938773b2
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2020-12-10 16:22:01 +0200

    Add files via upload

---
+++ TradingCompany/WF/Models/Concrete/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WF.Models.Interfaces;
+
+namespace WF.Models.Concrete
+{
+    public class ApplicationUser : IApplicationUser
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+    }
+}
---
+++ TradingCompany/WF/Models/Interfaces/IApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF.Models.Interfaces
+{
+    public interface IApplicationUser
+    {
+        int UserId { get; set; }
+        string Login { get; set; }
+    }
+}
