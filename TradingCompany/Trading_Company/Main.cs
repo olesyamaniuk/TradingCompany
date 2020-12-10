c81a6f74ad899a6a34d30ae8ec750d92e0c140c4
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2020-12-10 16:11:02 +0200

    Add files via upload

---
+++ TradingCompany/Trading_Company/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trading_Company
+{
+    class Programm
+    {
+        static void Main()
+        {
+            Console.WriteLine("123");
+        }
+
+    }
+}
